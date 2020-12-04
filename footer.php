6079a0b9c06b9d978d14eac1aa1ba0b2c0f18c79
conference20 <74963963+conference20@users.noreply.github.com>
2020-12-04 17:43:08 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-</div>
-<div class="footer-wrap text-center text-dark text-white pt-1" style="margin-top:0px;color:black;">
-    copyright &copy; - 2020 Powered By DataRitz Technology, <a href="our_team.php"> Designed & Developed By :AKS</a>
-    
-</div>
-</body>
-</html>
