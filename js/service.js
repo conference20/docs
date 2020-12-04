21f6e43cbce1edde405b703e47f248362c649719
conference20 <74963963+conference20@users.noreply.github.com>
2020-12-04 17:46:07 +0530

    Add files via upload

---
+++ js/service.js
@@ -0,0 +1,65 @@
+document.write("\n" +
+    "<section id=\"service\" class=\"service section\">\n" +
+    "    <div class=\"container\">\n" +
+    "        <div class=\"row\">\n" +
+    "            <div class=\"col-sm-6 col-md-3 wow fadeInLeft\">\n" +
+    "                <div class=\"block\">\n" +
+    "                    <i class=\"fas fa-file-upload\"></i>\n" +
+    "                    <h3><a href=\"indexx/callforpapers.html\">Call for Papers</a></h3>\n" +
+    "                    <p>We invite you to submit papers and also request you to disseminate this CFP to your\n" +
+    "                        colleagues.</p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-sm-6 col-md-3 wow fadeInLeft\" data-wow-delay=\"0.3s\">\n" +
+    "                <div class=\"block\">\n" +
+    "                    <i class=\"far fa-calendar-alt\"></i>\n" +
+    "                    <h3><a href=\"indexx/dates.html\">Important Dates</a></h3>\n" +
+    "                    <p>Submission Start : 01<sup>th</sup> November  2020<br>\n" +
+    "                        Submission Deadline : 4<sup>th</sup>  March 2020</p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-sm-6 col-md-3 wow fadeInLeft\" data-wow-delay=\"0.6s\">\n" +
+    "                <div class=\"block\">\n" +
+    "                    <i class=\"fas fa-file-signature\"></i>\n" +
+    "                    <h3><a href=\"indexx/registration.html\">Registration</a></h3>\n" +
+    "                    <p>Last Date of registration is 4<sup>th</sup> March 2020</p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-sm-6 col-md-3 wow fadeInLeft\" data-wow-delay=\"0.9s\">\n" +
+    "                <div class=\"block\">\n" +
+    "                    <i class=\"fas fa-gavel\"></i>\n" +
+    "                    <h3><a href=\"#\">Proceedings</a></h3>\n" +
+    "                    <p>Proceedings of International Conference on System Modelling Simulation & Intelligent Computing. </p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-sm-6 col-md-3 wow fadeInLeft\">\n" +
+    "                <div class=\"block\">\n" +
+    "                    <i class=\"fas fa-clipboard-list\"></i>\n" +
+    "                    <h3><a href=\"#\">Advisory Board</a></h3>\n" +
+    "                    <p>Details about the members of our Advisory Board</p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-sm-6 col-md-3 wow fadeInLeft\" data-wow-delay=\"0.3s\">\n" +
+    "                <div class=\"block\">\n" +
+    "                    <i class=\"fas fa-users\"></i>\n" +
+    "                    <h3><a href=\"#\">Organizing Committee</a></h3>\n" +
+    "                    <p>Details about the members of our committee</p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-sm-6 col-md-3 wow fadeInLeft\" data-wow-delay=\"0.6s\">\n" +
+    "                <div class=\"block\">\n" +
+    "                    <i class=\"fas fa-globe-asia\"></i>\n" +
+    "                    <h3><a href=\"#\">Technical Program Committee</a></h3>\n" +
+    "                    <p>Details about the members of our Technical Program Committee</p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"col-sm-6 col-md-3 wow fadeInLeft\" data-wow-delay=\"0.9s\">\n" +
+    "                <div class=\"block\">\n" +
+    "                    <i class=\"fas fa-file\"></i>\n" +
+    "                    <h3><a href=\"#\">Paper Submission</a></h3>\n" +
+    "                    <p>All the papers must be submitted through on-line paper submission process only</p>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</section>\n");
